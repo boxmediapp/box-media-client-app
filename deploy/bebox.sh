df1a4116c0c5bff72d922293b77621297e15e216
Dilshat Hewzulla <hewzulla@gmail.com>
2016-10-24 00:14:12 +0100

    development

--- deploy/bebox.sh
+++ deploy/bebox.sh
@@ -1,9 +1,9 @@
 git add . --all
 git commit -m "development"
-git push home
+git push origin
 git checkout master
 git merge develop
-git push home
+git push origin
 git checkout develop
 ssh ec2-user@be.boxnetwork.co.uk 'cd box-media-client-app && git pull origin'
 
--- deploy/box.sh
+++ deploy/box.sh
@@ -1,9 +1,9 @@
 git add . --all
 git commit -m "development"
-git push home
+git push origin
 git checkout master
 git merge develop
-git push home
+git push origin
 git tag -a $1 -m "release $1"
 git checkout develop
 ssh ec2-user@boxnetwork.co.uk 'cd box-media-client-app && git pull origin'
