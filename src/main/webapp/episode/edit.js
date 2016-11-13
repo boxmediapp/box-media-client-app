4c16be5d24485b2f9ebef64f178e505d83c9e884
Dilshat Hewzulla <hewzulla@gmail.com>
2016-11-13 16:22:17 +0000

    development

--- src/main/webapp/episode/edit.js
+++ src/main/webapp/episode/edit.js
@@ -406,7 +406,37 @@ jQuery(document).ready(function ($) {
 
             $("#fileUploaderDialog .fileuploader").uploadFile({
                     url: data.baseURL,
-                    paramName:"dilshat",
+                    type: 'POST',
+                    datatype: 'xml',
+                    add: function (event, data) {
+                        // Message on unLoad.
+                        window.onbeforeunload = function() {
+                            return 'You have unsaved changes.';
+                        };
+                        // Submit
+                        data.submit();
+                    },
+                    end: function(e, data) {
+                        // onSend
+                    },
+                    progress: function(e, data){
+                        // This is what makes everything really cool, thanks to that callback
+                        // you can now update the progress bar based on the upload progress.
+                        var percent = Math.round((data.loaded / data.total) * 100);
+                        $('.bar').css('width', percent + '%');
+                    },
+                    fail: function(e, data) {
+                        // Remove 'unsaved changes' message.
+                        window.onbeforeunload = null;
+                    },
+                    success: function(data) {
+                        // onSuccess
+                    },
+                    done: function (event, data) {
+                        // Fill the name field with the file's name.
+                        $('#upload_original_name').val(data.originalFiles[0].name);
+                        $('#upload_custom_name').val(data.originalFiles[0].name);
+                    },
                     filename: data.path,                   
                     formData:{
                         "key":data.path,
