68518c3f7a710d8a289c179bc17b99ddb9792894
Dilshat Hewzulla <hewzulla@gmail.com>
2016-11-17 23:22:07 +0000

    development

--- src/main/webapp/episode/edit.js
+++ src/main/webapp/episode/edit.js
@@ -299,7 +299,7 @@ jQuery(document).ready(function ($) {
                     }
                 }
                 console.log(missingField+" ---> "+inutSelection);
-                $(inutSelection).addClass("requiredFieldMissing");
+                $(inutSelection).parent().addClass("requiredFieldMissing");
             }
         };
         boxservice.episode.editpage.intPlayVideo = function (episode) {
