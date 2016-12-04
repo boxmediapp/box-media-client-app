602c2af46a5acef16aa3a794fa33017ceb8cd708
Dilshat Hewzulla <hewzulla@gmail.com>
2016-12-04 21:03:13 +0000

    development

--- src/main/webapp/main/util.js
+++ src/main/webapp/main/util.js
@@ -655,7 +655,7 @@ boxservice.util.isArrayDifferent=function(array1, array2){
       boxservice.util.page.load=function(page){    	    
       	return $.ajax({
   		    type: "GET",
-  		    url: page+"?version=110",
+  		    url: page+"?version=111",
   		    dataType: "html"				    
   		   });
       };
