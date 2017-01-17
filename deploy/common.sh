ceefd9dceb44c38a703eba779913e8cb0fbfc2fa
Dilshat Hewzulla <hewzulla@gmail.com>
2017-01-17 16:01:03 +0000

    release

---
+++ deploy/common.sh
@@ -0,0 +1,44 @@
+
+getProjectSettings(){
+  export artefcactbasename="box-media-client-app"
+  export targetboxmediaclientdir=box-media-client-app/src/main/webapp
+}
+
+getProjectVersionFromPom(){
+  projectversion=`grep -A 0 -B 2 "<packaging>" pom.xml  | grep version  | cut -d\> -f 2 | cut -d\< -f 1`
+  export projectversion  
+}
+getArtefactName(){
+  export artefactfilename="$artefcactbasename-$projectversion.zip"
+  export sourceartefactpath="package/target/$artefactfilename"
+  export targetartefactpath="$targetboxmediaclientdir/$artefactfilename"
+}
+
+executeScriptOnServer(){
+   scp $1 $targetuser@$targetserver:boxmediadeployscript.sh   
+   ssh $targetuser@$targetserver 'chmod u+x boxmediadeployscript.sh' 
+   ssh $targetuser@$targetserver './boxmediadeployscript.sh'
+   ssh $targetuser@$targetserver "rm boxmediadeployscript.sh"
+}
+
+createTargetDirectory(){
+  echo "mkdir -p $targetboxmediaclientdir" > /tmp/preparedeployboxmediapp.sh
+  executeScriptOnServer /tmp/preparedeployboxmediapp.sh
+}
+
+uploadArtefact(){
+    scp $sourceartefactpath $targetartefactpath
+}
+
+deploArtefact(){
+  uploadArtefact()
+  echo "cd $targetboxmediaclientdir" > /tmp/deployboxmediapp.sh
+  echo "unzip -o $artefactfilename" >> /tmp/deployboxmediapp.sh
+  echo "rm  $artefactfilename" >> /tmp/deployboxmediapp.sh
+  echo sed -i -e "s,@@@version@@@,$projectversion,g" index.html >> /tmp/deployboxmediapp.sh
+  executeScriptOnServer /tmp/deployboxmediapp.sh
+}
+  
+ 
+
+
--- deploy/release.sh
+++ deploy/release.sh
@@ -1,20 +1,18 @@
-artefcactbasename="box-media-client-app"
+source deploy/common.sh
 
 mvn jgitflow:release-start
 mvn jgitflow:release-finish
 
+git checkout  master
+
+getProjectVersionFromPom
+getProjectSettings
+export targetboxmediaclientdir="box-deployments"
+getArtefactName
+
+uploadArtefact
 
 
-
-
-
-
-git checkout checkout master
-
-projectversion=`grep -A 0 -B 2 "<packaging>" pom.xml  | grep version  | cut -d\> -f 2 | cut -d\< -f 1`
-artefactfilename="$artefcactbasename-$projectversion.zip"
-
-scp package/target/$artefactfilename davran@userver:box-deployments/
 
 
 
