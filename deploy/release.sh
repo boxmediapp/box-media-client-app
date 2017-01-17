e40fbc2703c90d90db89e9c57cac8df3a0919ad8
Dilshat Hewzulla <hewzulla@gmail.com>
2017-01-17 16:49:07 +0000

    releasing

--- deploy/release.sh
+++ deploy/release.sh
@@ -15,6 +15,16 @@ targetuser=$devtargetuser
 targetserver=$devtargetserver
 uploadArtefact
 
+git checkout develop
+
+echo "deploy/deploy.sh $devtargetserver $devtargetuser $projectversion" > deploy/latest_to_dev.sh
+chmod u+x deploy/latest_to_dev.sh
+
+echo "deploy/deploy.sh $boxserver $boxuser $projectversion" > deploy/latest_to_box.sh
+chmod u+x deploy/latest_to_box.sh
+
+echo "deploy/deploy.sh $beboxserver $beboxuser $projectversion" > deploy/latest_to_bebox.sh
+chmod u+x deploy/latest_to_bebox.sh
 
 
 
