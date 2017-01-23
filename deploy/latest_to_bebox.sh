1355b3804a20fbfba422b041a29bc5653f13a3db
Dilshat Hewzulla <hewzulla@gmail.com>
2017-01-23 17:02:40 +0000

    releasing

--- deploy/latest_to_bebox.sh
+++ deploy/latest_to_bebox.sh
@@ -1,1 +1,1 @@
-deploy/deploy.sh be.boxnetwork.co.uk ec2-user 2.5.4
+deploy/deploy.sh be.boxnetwork.co.uk ec2-user 2.5.5
--- deploy/latest_to_dev.sh
+++ deploy/latest_to_dev.sh
@@ -1,1 +1,1 @@
-deploy/deploy.sh userver davran 2.5.4
+deploy/deploy.sh userver davran 2.5.5
