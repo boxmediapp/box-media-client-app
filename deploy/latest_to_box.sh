a5618a06076bbdb2cba6a181133d547e8a145432
Dilshat Hewzulla <hewzulla@gmail.com>
2017-01-30 14:26:39 +0000

    releasing

--- deploy/latest_to_box.sh
+++ deploy/latest_to_box.sh
@@ -1,1 +1,1 @@
-deploy/deploy.sh boxnetwork.co.uk ec2-user 2.9.1
+deploy/deploy.sh boxnetwork.co.uk ec2-user 2.9.2
--- deploy/latest_to_dev.sh
+++ deploy/latest_to_dev.sh
@@ -1,1 +1,1 @@
-deploy/deploy.sh userver davran 2.9.1
+deploy/deploy.sh userver davran 2.9.2
