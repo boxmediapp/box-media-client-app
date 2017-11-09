0c6c71b252a888314dc3b070c93effedfaca8594
Dilshat Hewzulla <hewzulla@gmail.com>
2017-11-09 00:21:45 +0000

    added filter for the channelId

--- deploy/deploy_to_dubuntu.sh
+++ deploy/deploy_to_dubuntu.sh
@@ -1,3 +1,3 @@
 source deploy/prod.sh
-echo "deploying the version 3.0.7 to dilshat@dubuntu using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh dubuntu dilshat 3.0.7
+echo "deploying the version 3.0.9-SNAPSHOT to dilshat@dubuntu using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh dubuntu dilshat 3.0.9-SNAPSHOT
