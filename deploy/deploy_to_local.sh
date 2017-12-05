de95a3ebf146f9ea2274cae456786f37094b312d
Dilshat Hewzulla <hewzulla@gmail.com>
2017-12-05 21:30:11 +0000

    media application

--- deploy/deploy_to_local.sh
+++ deploy/deploy_to_local.sh
@@ -1,3 +1,3 @@
 source deploy/prod.sh
-echo "deploying the version 3.1.1 to local@local using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
-deploy/local.sh local local 3.1.1
+echo "deploying the version 3.1.2 to local@local using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
+deploy/local.sh local local 3.1.2
