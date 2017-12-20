4b0ba009edb47d0e01c0cc0ff4dbcc0364a469cc
Dilshat Hewzulla <hewzulla@gmail.com>
2017-12-20 00:28:40 +0000

    deploy scripts

--- deploy/deploy_to_local.sh
+++ deploy/deploy_to_local.sh
@@ -1,3 +1,3 @@
 source deploy/prod.sh
-echo "deploying the version 3.2.3 to local@local using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
-deploy/local.sh local local 3.2.3
+echo "deploying the version 3.2.4-SNAPSHOT to local@local using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
+deploy/local.sh local local 3.2.4-SNAPSHOT
