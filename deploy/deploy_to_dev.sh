a4d98432d6b14b9e2ddd0957f702efee57ad6d86
Dilshat Hewzulla <hewzulla@gmail.com>
2018-02-16 17:09:07 +0000

    releasing

--- deploy/deploy_to_dev.sh
+++ deploy/deploy_to_dev.sh
@@ -1,3 +1,3 @@
 source deploy/prod.sh
-echo "deploying the version 4.0.5 to davran@userver using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh userver davran 4.0.5
+echo "deploying the version 4.1.0 to davran@userver using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh userver davran 4.1.0
