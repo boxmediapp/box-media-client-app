68210639cf16dae5ae140510fb01de52e8d93ec2
Dilshat Hewzulla <hewzulla@gmail.com>
2018-03-06 01:48:57 +0000

    develoyment

--- deploy/deploy_to_image.sh
+++ deploy/deploy_to_image.sh
@@ -1,3 +1,3 @@
 source deploy/prod.sh
-echo "deploying the version 4.2.0 to ec2-user@image.boxnetwork.co.uk using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh image.boxnetwork.co.uk ec2-user 4.2.0
+echo "deploying the version 4.2.1 to ec2-user@image.boxnetwork.co.uk using the property file deploy/prod.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh image.boxnetwork.co.uk ec2-user 4.2.1
